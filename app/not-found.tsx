d36d9ba926498358e62794fecf1b24444051477d
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-23 00:50:24 +0900

    Add: not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="font-bold text-2xl" role="alert">
+          ページが見つかりませんでした。
+        </h1>
+      </div>
+    </>
+  );
+}

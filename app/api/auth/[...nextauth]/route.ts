64e0ae63b056bcf261cabb48cf1a9eecbc28dfb3
MSK1206 <phantom7216@yahoo.co.jp>
2023-11-01 14:43:44 +0900

    Fix: api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,35 @@
 import NextAuth from 'next-auth';
-import { authConfig } from '@/app/api/Auth';
+import type { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
-export const authHandler = NextAuth(authConfig);
+const authOptions: NextAuthOptions = {
+  session: { strategy: 'jwt' },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string,
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
+  pages: {
+    signIn: '/login',
+    signOut: '/',
+  },
+};
 
-export { authHandler as GET, authHandler as POST };
+export default NextAuth(authOptions);
